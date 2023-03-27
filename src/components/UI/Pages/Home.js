ad271517a91254ac6897247ab71189fc67eb0018
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-03-27 13:50:42 +0000

    Mar 27, 2023, 2:50 PM

--- src/components/UI/Pages/Home.js
+++ src/components/UI/Pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
-    return <div>
-      
-  </div>;
+  return (
+    <Container>
+      <Row>
+              <Col>
+              </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
