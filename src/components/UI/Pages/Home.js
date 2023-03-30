02fa6ce87b3150d67683bd132dc2f31d201ff195
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-03-30 08:59:39 +0000

    Mar 30, 2023, 9:59 AM

--- src/components/UI/Pages/Home.js
+++ src/components/UI/Pages/Home.js
@@ -82,6 +82,61 @@ const Home = () => {
               </Card>
             </Col>
           </Row>
+
+          <Row style={{ textAlign: "center" }}>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Text>
+                    <a href="/" className={classes.link}>
+                      <i
+                        className={`bi bi-telephone-inbound-fill ${classes.icon}`}
+                      ></i>
+                      <p>Buy Airtime</p>
+                    </a>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Text>
+                    <a href="/" className={classes.link}>
+                      <i className={`bi bi-wifi ${classes.icon}`}></i>
+                      <p>Buy Data</p>
+                    </a>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Text>
+                    <a href="/" className={classes.link}>
+                      <i className={`bi bi-lightbulb ${classes.icon}`}></i>
+                      <p>Buy Electricity</p>
+                    </a>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Text>
+                    <a href="/" className={classes.link}>
+                      <i className={`bi bi-tv ${classes.icon}`}></i>
+                      <p>Cable TV</p>
+                    </a>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
