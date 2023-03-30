ebfad44fab3edae429e28c6ff1b0f3ff9c7b0d51
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-03-30 09:28:10 +0000

    Mar 30, 2023, 10:28 AM

--- src/components/UI/Pages/Reason.js
+++ src/components/UI/Pages/Reason.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import classes from "./Reason.module.css";
 
 const Reason = () => {
-  return <div className={classes.reason}>Reason</div>;
+  return (
+    <Container fluid className={classes.reason}>
+          <Row>
+              <Col>
+                  <h2>
+              </Col>
+              <Col></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Reason;
