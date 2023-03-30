53e00ef8724aed78de312975281f76a7d6dc1237
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-03-30 09:31:31 +0000

    Mar 30, 2023, 10:31 AM

--- src/components/UI/Pages/Reason.js
+++ src/components/UI/Pages/Reason.js
@@ -10,9 +10,15 @@ const Reason = () => {
           <h2>Why Choose Us?</h2>
           <p>
             We offer instant recharge of Airtime, Databundle, CableTV (DStv,
-                      GOtv & Startimes), Electricity Bill Payment and more...
-                      <br/>We're Fast <br/>Automated Services checl100% Secured checlWe're Reliable
-            checlAlways Online checl24/7 Customer Support
+            GOtv & Startimes), Electricity Bill Payment and more...
+            <br />
+            We're Fast <br />
+            Automated Services <br />
+            100% Secured <br />
+            We're Reliable
+            <br />
+            Always Online <br />
+            24/7 Customer Support
           </p>
         </Col>
         <Col></Col>
