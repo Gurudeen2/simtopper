3edac854cb92a29d140bdce1686802b433bef743
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-03-30 09:30:48 +0000

    Mar 30, 2023, 10:30 AM

--- src/components/UI/Pages/Reason.js
+++ src/components/UI/Pages/Reason.js
@@ -5,11 +5,17 @@ import classes from "./Reason.module.css";
 const Reason = () => {
   return (
     <Container fluid className={classes.reason}>
-          <Row>
-              <Col>
-                  <h2>
-              </Col>
-              <Col></Col>
+      <Row>
+        <Col>
+          <h2>Why Choose Us?</h2>
+          <p>
+            We offer instant recharge of Airtime, Databundle, CableTV (DStv,
+                      GOtv & Startimes), Electricity Bill Payment and more...
+                      <br/>We're Fast <br/>Automated Services checl100% Secured checlWe're Reliable
+            checlAlways Online checl24/7 Customer Support
+          </p>
+        </Col>
+        <Col></Col>
       </Row>
     </Container>
   );
