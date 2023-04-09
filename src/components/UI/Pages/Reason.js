b1af92be87728d84780b12e9a25b632de45eb205
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-04-09 10:56:51 +0000

    Apr 9, 2023, 11:56 AM

--- src/components/UI/Pages/Reason.js
+++ src/components/UI/Pages/Reason.js
@@ -26,6 +26,48 @@ const Reason = () => {
           <img src={image} className={classes["round-img"]} alt="choose us" />
         </Col>
       </Row>
+
+      <Row>
+        <Col className={classes["justify-content2"]}>
+          <h2>Why Choose Us?</h2>
+          <p>
+            We offer instant recharge of Airtime, Databundle, CableTV (DStv,
+            GOtv & Startimes), Electricity Bill Payment and more...
+            <br />
+            We're Fast <br />
+            Automated Services <br />
+            100% Secured <br />
+            We're Reliable
+            <br />
+            Always Online <br />
+            24/7 Customer Support
+          </p>
+        </Col>
+        <Col className={classes["justify-content"]}>
+          <img src={image} className={classes["round-img"]} alt="choose us" />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className={classes["justify-content2"]}>
+          <h2>Why Choose Us?</h2>
+          <p>
+            We offer instant recharge of Airtime, Databundle, CableTV (DStv,
+            GOtv & Startimes), Electricity Bill Payment and more...
+            <br />
+            We're Fast <br />
+            Automated Services <br />
+            100% Secured <br />
+            We're Reliable
+            <br />
+            Always Online <br />
+            24/7 Customer Support
+          </p>
+        </Col>
+        <Col className={classes["justify-content"]}>
+          <img src={image} className={classes["round-img"]} alt="choose us" />
+        </Col>
+      </Row>
     </Container>
   );
 };
