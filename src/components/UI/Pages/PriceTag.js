959434402344b83f7862ad5ab0f5f7a5324a957d
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-04-11 07:52:33 +0000

    Apr 11, 2023, 8:52 AM

--- src/components/UI/Pages/PriceTag.js
+++ src/components/UI/Pages/PriceTag.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, Container, Row } from "react-bootstrap";
+import img from "../../../assets/images/photo-1624411024074-18a756682b50.jfif";
 
 const PriceTag = () => {
   return (
@@ -7,11 +8,14 @@ const PriceTag = () => {
       <Row>
         <Col>
           <Card>
-                      <Card.Body>
-                          <Card.
-              <Card.Text>
-               
-              </Card.Text>
+            <Card.Body>
+              <Card.Header>
+                <Card.Img src={img} />
+              </Card.Header>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text></Card.Text>
             </Card.Body>
           </Card>
         </Col>
