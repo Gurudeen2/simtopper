d0fce8c2e5e5989e83dc6c915ec551294ad90656
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-04-18 14:38:05 +0000

    Apr 18, 2023, 3:38 PM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import LandingPage from "./components/HomePage/LandingPage";
+// import LandingPage from "./components/HomePage/LandingPage";
 import Footer from "./components/UI/Footer/Footer";
 import Header from "./components/UI/Header/Header";
 import Register from "./components/User/Register";
