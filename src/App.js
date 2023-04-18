1022096e8a5764daec946c1c57eb5a449d1fd6e3
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-04-18 13:48:02 +0000

    Apr 18, 2023, 2:48 PM

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div>
       <Header />
-      <LandingPage />
+      {/* <LandingPage /> */}
 
-      {/* <Register /> */}
+      <Register />
       <Footer />
     </div>
   );
--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -3,34 +3,37 @@ import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 function Register() {
   return (
-    <Container>
-      <Row>
-        <Col sm="12"></Col>
-        <Col sm="12">
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+    <Container fluid>
+      <div ></div>
+      <Container>
+        <Row>
+          <Col sm="12"></Col>
+          <Col sm="12">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter username" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Remember password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Col>
-      </Row>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
