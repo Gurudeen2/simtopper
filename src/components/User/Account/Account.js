7d7d775049dac7bda08c8634601d9c03b9eca63e
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-05-10 17:09:53 +0100

    update commit

--- src/components/User/Account/Account.js
+++ src/components/User/Account/Account.js
@@ -1,30 +1,28 @@
 import React from "react";
 import { Col, Container, Row, Card } from "react-bootstrap";
 
+import classes from "./Account.module.css";
+
 function Account() {
   return (
     <>
-      <Container>
+      <Container fluid className={classes["account-container"]}>
         <Row>
           <Col md="12" sm="12" lg="12" xs="12">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="https://via.placeholder.com/286x180"
-              />
-              <Card.Body>
+            <Card className="shadow-sm p-3 mb-5 bg-white rounded border-0">
+              <h3>
+                Akeem Fatai <a href="/" classNmae={classes.achor}>Logout</a>
+              </h3>
+
+              {/* <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </Card.Text>
 
-              </Card.Body>
+              </Card.Body> */}
             </Card>
-            <h3>Name</h3>{" "}
-            <span>
-              (<a href="/">Log Out</a>)
-            </span>
           </Col>
         </Row>
         <Row>
