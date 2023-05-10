15c91e23b53f4cb3cd226e932bd9e6bf15bbec84
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-05-10 15:14:12 +0000

    May 10, 2023, 4:14 PM

--- src/components/User/Account/Account.js
+++ src/components/User/Account/Account.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Card } from "react-bootstrap";
 
 function Account() {
   return (
@@ -7,6 +7,20 @@ function Account() {
       <Container>
         <Row>
           <Col md="12" sm="12" lg="12" xs="12">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://via.placeholder.com/286x180"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+
+              </Card.Body>
+            </Card>
             <h3>Name</h3>{" "}
             <span>
               (<a href="/">Log Out</a>)
