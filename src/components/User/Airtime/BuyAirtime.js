47edcf4d3afd4e5b45b1c1ab63812a3ef9e4cfe2
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-05-15 13:43:55 +0000

    May 15, 2023, 2:43 PM

--- src/components/User/Airtime/BuyAirtime.js
+++ src/components/User/Airtime/BuyAirtime.js
@@ -37,7 +37,7 @@ const BuyAirtime = () => {
                 // defaultValue=""
               />
               <Form.Control.Feedback type="invalid">
-                Please enter Grade!
+                Please enter Grade
               </Form.Control.Feedback>
             </Form.Group>
           </Col>
