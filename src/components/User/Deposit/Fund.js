1ea09a0e455491c6d4e09696f2a2220bd375cb68
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-09-21 03:04:55 +0100

    deposit frontend issue with webpayCheckout  solved

--- src/components/User/Deposit/Fund.js
+++ src/components/User/Deposit/Fund.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Form } from "react-bootstrap";
 import { Route, Routes, Link } from "react-router-dom";
 import InterSwitch from "./InterSwitch";
 
 const Fund = () => {
+  const [amount, setAmount] = useState("")
   return (
     <>
       <h3>Fund Your Account </h3>
@@ -16,13 +17,14 @@ const Fund = () => {
               placeholder="Amount"
               name="amount"
               required
+              onChange={(e)=>setAmount(e.target.value)}
             />
           </Form.Group>
         </Col>
         <Col>
           <Link
             className="btn btn-sm btn-primary"
-            to="/account/deposit/payment/100"
+            to={`/account/deposit/payment/${amount}`}
           >
             Proceed Payment
           </Link>
